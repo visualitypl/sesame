b36a7519318dea77b2ce0691aaa35a8c9e3a0583
Sakir Temel <sakirtemel@gmail.com>
2015-02-20 13:34:15 +0100

    Close the relay after 1 second

--- app.js
+++ app.js
@@ -4,27 +4,22 @@ var app = express()
 var gpio = require("pi-gpio");
 var pin = 11;
 
-/* gpio */
-gpio.close(pin);                     // Close pin 16
-
-gpio.open(pin, "output", function(err) {     // Open pin 16 for output
-    gpio.write(pin, 1, function() {});
-});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.get('/open', function (req, res) {
-  gpio.write(pin, 0, function() {});
-  console.log('open called!')
-})
+    gpio.open(pin, "output", function(err) {});
+    gpio.write(pin, 0, function() {});
 
-app.get('/close', function (req, res) {
-  gpio.write(pin, 1, function() {});
-  console.log('close called!')
-})
+    setTimeout(function() {
+        gpio.write(pin, 1, function(){});
+    }, 1000);
+    gpio.close(pin);
 
+    console.log('open called!')
+});
 var server = app.listen(3000, function () {
 
   var host = server.address().address
