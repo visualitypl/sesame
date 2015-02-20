3087de6bb2ae1110001753e3bbfb88115c37487d
Sakir Temel <sakirtemel@gmail.com>
2015-02-20 13:41:07 +0100

    Always set the connection open

--- app.js
+++ app.js
@@ -4,23 +4,26 @@ var app = express()
 var gpio = require("pi-gpio");
 var pin = 11;
 
+/* gpio */
+gpio.close(pin);                     // Close pin 16
+
+gpio.open(pin, "output", function(err) {     // Open pin 16 for output
+    gpio.write(pin, 1, function() {});
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
 app.get('/open', function (req, res) {
-    console.log('open called!')
-    gpio.open(pin, "output", function(err) {});
-    gpio.write(pin, 0, function() {});
+  console.log('open called');
+  gpio.write(pin, 0, function() {});
 
     setTimeout(function() {
         gpio.write(pin, 1, function(){});
+      console.log('closed!')
     }, 1000);
-    console.log('closed!')
-
-    gpio.close(pin);
-    res.send('done!')
+    res.send('done!');
 
 });
 var server = app.listen(3000, function () {
