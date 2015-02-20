627ac8851070a4610ee07d54346bc88d69626f16
Sakir Temel <sakirtemel@gmail.com>
2015-02-20 13:25:45 +0100

    Gpio init
    - Gpio init
    
    - Sample app

--- app.js
+++ app.js
@@ -1,60 +1,35 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var app = express()
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var gpio = require("pi-gpio");
+var pin = 11;
 
-var app = express();
+/* gpio */
+gpio.close(pin);                     // Close pin 16
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+gpio.open(pin, "output", function(err) {     // Open pin 16 for output
+    gpio.write(pin, 1, function() {});
 });
 
-// error handlers
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+app.get('/open', function (req, res) {
+  gpio.write(pin, 0, function() {});
+  console.log('open called!')
+})
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+app.get('/close', function (req, res) {
+  gpio.write(pin, 1, function() {});
+  console.log('close called!')
+})
 
+var server = app.listen(3000, function () {
 
-module.exports = app;
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
